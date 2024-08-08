a657e912ad6c4119dcc377b8b815e62200433c49
yehan-cardenas <yehancardenas@gmail.com>
2024-08-08 14:51:05 -0500

    Update resultads.php

--- resultads.php
+++ resultads.php
@@ -1,10 +1,20 @@
 <?php
-$identificacion= $_POST ['identificacion'];
-$nomapellidos= $_POST ['nomapellido'];
-$cargo= $_POST ['cargo'];
-$salario= $_POST ['salario'];
-echo "la identificacion del empleado es :   ".$identificacion;
-echo "los nombres y apellidos del empleado son :   ".$nomapellidos;
-echo "el cargo del empleado es :   ".$cargo;
-echo "el salario del empleado es :   ".$salario;
+$producto_1= $_POST ['producto_1'];
+$precio_1= $_POST ['producto_2'];
+$producto_3= $_POST ['producto_3'];
+$precio_1= $_POST ['precio_1'];
+$precio_2= $_POST ['precio_2'];
+$precio_3= $_POST ['precio_3'];
+$suma=$precio_1+$precio_2+$precio_3;
+$descuento=$suma-0.16%;
+echo "producto 1:   ".$producto_1.'= '.$precio_1;
+echo '<br>'
+echo "producto 2:   ".$producto_2.'= '.$precio_2;
+echo '<br>'
+echo "producto 3:   ".$producto_3.'= '.$precio_3;
+echo '<br>'
+echo "la suma de los productos es:   ".$suma;
+echo '<br>'
+echo "el total a pagar es=    ".$descuento;
+;
 ?>
