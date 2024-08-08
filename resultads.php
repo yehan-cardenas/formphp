c82b21ac791ae67af7d3f67fabcc6fa43b76e7cc
yehan-cardenas <yehancardenas@gmail.com>
2024-08-08 14:21:14 -0500

    Create resultads.php

---
+++ resultads.php
@@ -0,0 +1,10 @@
+<?php
+$identificacion= $_POST ['identificacion'];
+$nomapellidos= $_POST ['nomapellido'];
+$cargo= $_POST ['cargo'];
+$salario= $_POST ['salario'];
+echo "la identificacion del empleado es :   ".$identificacion;
+echo "los nombres y apellidos del empleado son :   ".$nomapellidos;
+echo "el cargo del empleado es :   ".$cargo;
+echo "el salario del empleado es :   ".$salario;
+?>
